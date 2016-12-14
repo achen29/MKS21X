a48d5b3103ff547d0f1a78c21484700b1a958798
achen29 <achen28@stuy.edu>
2016-12-14 14:50:27 -0500

    Add files via upload

--- Sorts.java
+++ Sorts.java
@@ -3,7 +3,7 @@ public class Sorts{
 	return "01.Chen.Alvin";
     }
     public static void selectionSort(int[] data){
-	for(int i = 0; i < data.length; i ++){
+	for(int i = 0; i < data.size(); i ++){
 	    int n = 0;
 	    int x = i;
 	    for(int m = 0; m < data.length - i; m++){
@@ -13,6 +13,18 @@ public class Sorts{
 		}
 		data[x] = data[i];
 		data[i]= n;
+	    }
+	}
+    }
+    public static void insertionSort(int[]data){
+	for(int first = 0; first < data.length; first++){
+	    int temp = data[first];
+	    int towhere = first;
+	    for(int comparing = first; comparing > 0; comparing --){
+		if(temp < data[comparing]){
+		    data[comparing + 1] = data[comparing];
+		    towhere=comparing;
+		} 
 	    }
 	}
     }
