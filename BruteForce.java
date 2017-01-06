bc1e3259b3cea20355a40f39c7ed6b2824ad3c57
achen29 <achen28@stuy.edu>
2017-01-06 01:11:36 -0500

    Add files via upload

--- BruteForce.java
+++ BruteForce.java
@@ -1,6 +1,20 @@
 public class BruteForce{
-    ArrayList<Routes> permutations = new ArrayList<Routes>();
-    public class(ArrayList<cities> a){
+    private ArrayList<Routes> permutations = new ArrayList<Routes>();
+    public class (ArrayList<Cities> a ){
     }
-public 
-}
+    public void PermSolver(ArrayList<Cities> a){
+	ArrayList<Cities> b = ArrayList<Cities> a;
+	/** Ex. {C1, C2, C3, C4}
+	    Max : 3 2 1 0|
+	          0 0 0 0| C1 C2 C3 C4
+		  0 0 1 0| C1 C2 C4 C3
+		  0 1 0 0| C1 C3 C2 C4
+		  0 1 1 0| C1 C3 C4 C2
+		  0 2 0 0| C1 C4 C2 C3
+		  0 2 1 0| C1 C4 C3 C2
+		  ....
+		  ....
+		  ....
+		  ....
+	
+    }
